32eeb0802b309b6c9205447e8798813dd01c15ca
SlaviHristev <Slavihristev97@gmail.com>
2024-06-29 19:18:42 +0300

    Made my initial home page, will add more stuff after I connect with api

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar'
+import Layout from './pages/Layout/Layout'
+import Home from './pages/Home/Home'
 
 function App() {
- 
+ const router = createBrowserRouter([
+  {
+    path:'/',
+    element:(
+      <Layout/>
+    ),
+    children:[
+      {
+        path:'/',
+        element: <Home/>
+      },
+    ]
+  }
+ ])
   return (
     <>
-     <Navbar/>
+     <RouterProvider router={router}/>
     </>
   )
 }
