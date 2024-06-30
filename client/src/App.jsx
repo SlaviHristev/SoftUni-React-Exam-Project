6c8842fea03371e202024384964cf87f72cd5d2e
SlaviHristev <Slavihristev97@gmail.com>
2024-06-30 11:10:54 +0300

    Created Catalog page, will style and add functionality later

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Layout from './pages/Layout/Layout'
 import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Register from './pages/Register/Register'
+import Catalog from './pages/Catalog/Catalog'
 
 function App() {
   const router = createBrowserRouter([
@@ -23,6 +24,10 @@ function App() {
         {
           path: '/register',
           element: <Register />
+        },
+        {
+          path:'/catalog',
+          element: <Catalog/>
         }
       ]
     }
