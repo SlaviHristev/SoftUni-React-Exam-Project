77b1fb00b5925e6a59e2494daf5e8025a4504c4b
SlaviHristev <Slavihristev97@gmail.com>
2024-07-07 12:54:25 +0300

    Created the Single page and styled it, will be back to add something more

--- client/src/pages/SinglePage/SinglePage.jsx
+++ client/src/pages/SinglePage/SinglePage.jsx
@@ -3,6 +3,7 @@ import './singlePage.scss'
 import apiRequest from '../../lib/apiRequest';
 import { useParams } from 'react-router-dom';
 import Slider from '../../components/Slider/Slider';
+import DOMPurify from 'dompurify'
 
 const SinglePage = () => {
 
@@ -14,14 +15,14 @@ const SinglePage = () => {
   useEffect(() => {
     const getPost = async () => {
       try {
-        setLoading(true); 
+        setLoading(true);
         const response = await apiRequest.get(`/posts/${id}`);
         setPost(response.data);
-        setLoading(false); 
+        setLoading(false);
       } catch (error) {
         console.error('Failed to fetch post:', error);
         setError('Failed to fetch post');
-        setLoading(false); 
+        setLoading(false);
       }
     };
 
@@ -36,7 +37,84 @@ const SinglePage = () => {
     <div className='singlePage'>
       <div className="details">
         <div className="wrapper">
-          <Slider images={post.images}/>
+          <Slider images={post.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{post.title}</h1>
+                <div className="city">
+                  <img src="/pin.png" alt="" />
+                  <p>City:</p>
+                  <span>{post.city}</span>
+                </div>
+                <div className="price">
+                  <p>Price:</p>
+                  $ {post.price}</div>
+              </div>
+              {/* <div className="user">TODO</div> */}
+            </div>
+            <div className="bottom" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(post.description) }}>
+              { }
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="features">
+        <div className="wrapper">
+          <p className="title">General</p>
+          <div className="listVertical">
+            <div className="feature">
+              <img src="/calendar.png" alt="" />
+              <div className="featurText">
+                <span>Year</span>
+                <p>{post.yearOfMake}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/engine.png" alt="" />
+              <div className="featureText">
+                <span>Engine</span>
+                <p>{post.fuelType}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/horsePower.png" alt="" />
+              <div className="featureText">
+                <span>Horse Power</span>
+                <p>{post.horsePower}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/category.png" alt="" />
+              <div className="featureText">
+                <span>Category</span>
+                <p>{post.category}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/transmission.png" alt="" />
+              <div className="featureText">
+                <span>Transmission</span>
+                <p>{post.transmission}</p>
+              </div>
+            </div>
+            <div className="feature">
+              <img src="/color.png" alt="" />
+              <div className="featureText">
+                <span>Color</span>
+                <p>{post.color}</p>
+              </div>
+            </div>
+          </div>
+          <div className="buttons">
+            <button>
+              <img src="/chat.png" alt="" />
+              Send a Message
+            </button>
+            <button>
+              <img src="/save.png" alt="" />Save Post
+            </button>
+          </div>
         </div>
       </div>
     </div>
