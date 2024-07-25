6cb0bf8ab37ecf6a2ec69bdc5eb42409e41aa7f0
SlaviHristev <Slavihristev97@gmail.com>
2024-07-25 12:04:10 +0300

    Added the info coming from the api to the user profile page

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getSavedPosts, getUser, savePost, updateUser } from '../controllers/user.js';
+import { getSavedPosts, getMyUser, savePost, updateUser, getOtherUser } from '../controllers/user.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = express.Router();
 
-router.get('/:id', getUser);
+router.get('/:id', getMyUser);
 router.get('/:id/saved', getSavedPosts);
+router.get('/profiles/:id', getOtherUser);
 router.post('/save/:id', savePost);
 router.put('/:id',verifyToken, updateUser);
 
--- client/src/pages/UserProfile/UserProfile.jsx
+++ client/src/pages/UserProfile/UserProfile.jsx
@@ -1,16 +1,66 @@
+import { useNavigate, useParams } from 'react-router-dom';
 import './userProfile.scss'
+import { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
+import Card from '../../components/Card/Card';
+import useError from '../../hooks/useError';
+import Spinner from '../../components/Spinner/Spinner';
 
 const UserProfile = () => {
+    const {id} = useParams();
+    const {currentUser} = useContext(AuthContext);
+    const [profile, SetProfile] = useState(null);
+    const [posts,SetPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    const { showError } = useError();
+
+    useEffect(() =>{
+
+        const fetchProfileInfo = async() =>{
+            try {
+                setLoading(true);
+                const profileRes = await apiRequest.get(`/users/profiles/${id}`);
+                const postRes = await apiRequest.get(`/posts/user/${id}`);
+                SetProfile(profileRes.data);
+                SetPosts(postRes.data);
+                setLoading(false)
+            } catch (error) {
+                console.log(error);
+                showError('Failed to fetch User info')
+                setLoading(false);
+            }
+        }
+
+        fetchProfileInfo();
+
+    },[id]);
+
+    if (loading) return <Spinner />;
   return (
     <div className='userProfile'>
         <div className="userInfo">
-            <img src='' alt="username" />
-            <h2>Username</h2>
-            <p>Email</p>
-            <button>Message</button>
+            <img src={profile.avatar || '/noavatar.jpg'} alt="" />
+            <h2>{profile.username}</h2>
+            <p>{profile.email}</p>
+            {
+                currentUser && (
+                    <button>Message</button>
+                )
+            }
         </div>
         <div className="profilePosts">
             <h3>Posts</h3>
+            {
+                posts.length > 0 ? (
+                    posts.map(post =>(
+                        <Card key={post._id} item={post} />
+                    ))
+                ): (
+                    <p>No posts available</p>
+                )
+            }
             
         </div>
     </div>
