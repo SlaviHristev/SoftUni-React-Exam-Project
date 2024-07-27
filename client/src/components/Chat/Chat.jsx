016719a33b4c04de5dcbe924dafd44673c3d7de5
SlaviHristev <Slavihristev97@gmail.com>
2024-07-27 12:33:44 +0300

    Fixed a few issues with chat component and added ref to scroll down to the last message

--- client/src/components/Chat/Chat.jsx
+++ client/src/components/Chat/Chat.jsx
@@ -1,90 +1,101 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useSocket from "../../hooks/useSocket";
 import './chat.scss'
 import apiRequest from "../../lib/apiRequest";
 import useError from "../../hooks/useError";
 
 const Chat = ({
-  receiver,
-  setIsChatOpen,
-  chatId,
-  currentUser
+    receiver,
+    setIsChatOpen,
+    chatId,
+    currentUser
 }) => {
 
-  const { showError } = useError();
-  const { socket, messages, sendMessage } = useSocket(currentUser._id);
-  const [newMessage, setNewMessage] = useState('');
-  const [chatMessages, setChatMessages] = useState([]);
+    const { showError } = useError();
+    const { socket, messages, sendMessage } = useSocket(currentUser._id);
+    const [newMessage, setNewMessage] = useState('');
+    const [chatMessages, setChatMessages] = useState([]);
+    const messageEndRef = useRef(null);
 
-  useEffect(() => {
-    const fetchChatHistory = async () => {
-      try {
-        const res = await apiRequest.get(`/chats/${chatId}`);
-        setChatMessages(res.data.messages);
-      } catch (error) {
-        console.log('Failed to fetch chat history:', error);
-        showError('Failed to fetch chat history');
-      }
+    useEffect(() => {
+        const fetchChatHistory = async () => {
+            try {
+                const res = await apiRequest.get(`/chats/${chatId}`);
+                setChatMessages(res.data.messages);
+            } catch (error) {
+                console.log('Failed to fetch chat history:', error);
+                showError('Failed to fetch chat history');
+            }
+        }
+        fetchChatHistory();
+    }, [chatId]);
+
+    useEffect(() => {
+        if (messages.length > 0) {
+            const latestMessage = messages[messages.length - 1];
+            setChatMessages(prevMessages => [...prevMessages, latestMessage]);
+        }
+    }, [messages]);
+
+    const handleSendMessage = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await apiRequest.post('/messages/send', {
+                chatId,
+                senderId: currentUser._id,
+                text: newMessage
+            });
+
+            sendMessage(receiver._id, newMessage);
+            setChatMessages([...chatMessages, res.data]);
+            setNewMessage('');
+        } catch (error) {
+            console.log('Failed to send messages:', error);
+            showError('Failed to send message');
+        }
     }
-    fetchChatHistory();
-  }, [chatId]);
+    useEffect(() => {
+        messageEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    }, [chatMessages])
 
-  useEffect(() => {
-    if (messages.length > 0) {
-      const latestMessage = messages[messages.length - 1];
-      setChatMessages(prevMessages => [...prevMessages, latestMessage]);
-    }
-  }, [messages]);
+    const getSenderId = (message) => {
+        return typeof message.senderId === 'object' ? message.senderId._id : message.senderId;
+    };
 
-  const handleSendMessage = async (e) => {
-    e.preventDefault();
+    return (
+        <div className='chat'>
+            <div className="chatBox">
+                <div className="top">
+                    <div className="user">
+                        <img src={receiver.avatar || '/noavatar.jpg'} alt="" />
+                        <span>{receiver.username}</span>
+                    </div>
+                    <span className='close' onClick={() => setIsChatOpen(false)}>X</span>
+                </div>
+                <div className="center">
+                    {chatMessages.map(message => (
+                        <div className="chatMessage" key={message._id}
+                            style={{
+                                alignSelf: getSenderId(message) === String(currentUser._id) ? 'flex-end' : 'flex-start',
+                                textAlign: getSenderId(message) === String(currentUser._id) ? "right" : "left"
 
-    try {
-      const res = await apiRequest.post('/messages/send', {
-        chatId,
-        senderId: currentUser._id,
-        text: newMessage
-      });
+                            }}
+                        >
+                            <p>{message.text}</p>
+                            <span>{new Date(message.createdAt).toLocaleTimeString()}</span>
+                        </div>
 
-      sendMessage(receiver._id, newMessage);
-      setChatMessages([...chatMessages, res.data]);
-      setNewMessage('');
-    } catch (error) {
-      console.log('Failed to send messages:', error);
-      showError('Failed to send message');
-    }
-  }
-
-  return (
-    <div className='chat'>
-      <div className="chatBox">
-        <div className="top">
-          <div className="user">
-            <img src={receiver.avatar || '/noavatar.jpg'} alt="" />
-            <span>{receiver.username}</span>
-          </div>
-          <span className='close' onClick={() => setIsChatOpen(false)}>X</span>
-        </div>
-        <div className="center">
-          {chatMessages.map(message => (
-            <div className="chatMessage" key={message._id}
-              style={{
-                alignSelf: message.senderId === currentUser._id ? 'flex-end' : 'flex-start',
-                textAlign: message.senderId === currentUser._id ? "right" : "left"
-              }}
-            >
-              <p>{message.text}</p>
-              <span>{new Date(message.createdAt).toLocaleTimeString()}</span>
+                    ))}
+                    <div ref={messageEndRef} />
+                </div>
+                <form className="bottom" onSubmit={handleSendMessage}>
+                    <textarea name='text' value={newMessage} onChange={(e) => setNewMessage(e.target.value)}></textarea>
+                    <button type='submit'>Send</button>
+                </form>
             </div>
-          ))}
         </div>
-        <form className="bottom" onSubmit={handleSendMessage}>
-          <textarea name='text' value={newMessage} onChange={(e) => setNewMessage(e.target.value)}></textarea>
-          <button type='submit'>Send</button>
-        </form>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Chat;
